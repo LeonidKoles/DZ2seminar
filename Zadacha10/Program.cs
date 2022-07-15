f58826280d3cd812ff64d33e02549fb6882c03a6
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-07-15 09:02:09 +0300

    Zadacha10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,13 @@
+﻿void Zadacha10()
+{
+Console.WriteLine("Введите трехзначное число");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+if (numb >= 100 && numb < 1000) 
+{
+    int numb2 = numb / 10 % 10;
+    Console.WriteLine(numb2);
+}
+else Console.WriteLine("Число не трехзначное!!!");
+}
+Zadacha10 ();
