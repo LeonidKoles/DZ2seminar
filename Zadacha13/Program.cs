330e10fe048d955d205b788d8670cc97234136f2
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-07-15 09:51:32 +0300

    Zadacha13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,18 @@
+﻿void Zadacha13()
+{
+
+    Console.WriteLine("Введите число");
+    int numb = Convert.ToInt32(Console.ReadLine());
+
+    if (numb > 99)
+    {
+        while (numb > 999)
+        {
+            numb = numb / 10;
+        }
+        int numb3 = numb % 10;
+        Console.WriteLine( numb3);
+    }
+    else Console.WriteLine("Третьей цифры нет!!!");
+}
+Zadacha13 ();
