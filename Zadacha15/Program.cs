9011ddd5a04be4fdeb63d025ba6162cf1672f2d4
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-07-15 10:20:58 +0300

    Zadacha15

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,17 @@
+﻿void Zadacha15 ()
+{
+    Console.WriteLine("Введите код дня недели");
+    int numb = Convert.ToInt32(Console.ReadLine());
+
+    if (numb < 8)
+    {
+        if (numb > 5 && numb <=7)
+        {
+            Console.WriteLine("Выходной день");
+        }
+        else Console.WriteLine("Будний день");
+    
+    }
+    else Console.WriteLine("Не корректный код");
+}
+Zadacha15 ();
